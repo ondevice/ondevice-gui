3258decd3e93aeb511808fb595db0bee5b233b71
Manuel Reithuber <git@manuel.reithuber.net>
2016-12-11 00:56:10 +0100

    DeviceList: moved _reload() out of the class (since it's a callback)

--- deviceList.js
+++ deviceList.js
@@ -1,5 +1,5 @@
 EventSource = require('./eventSource.js');
-RestClient = require('./restClient.js');
+restClient = require('./restClient.js');
 $ = require('jQuery');
 
 var instance = null;
@@ -11,6 +11,38 @@ function _clickHandler(ev) {
 	instance._selectedItem = this;
 	$(this).addClass('selected');
 	instance.ev.fire('deviceSelected', this.dataset['devId']);
+}
+
+function _reload(data) {
+	var $list = $('#deviceList');
+	var onlineDevices = 0;
+
+	instance.clear();
+
+	for (var device of data.devices) {
+	//	console.log(device);
+
+		var li = $('<li class="list-group-item">\n'
+			+ '<span class="icon icon-monitor pull-left" style="font-size:24px;padding-right:5px" />\n'
+			+ '<div class="media-body">\n'
+			+ '<strong>device name</strong>\n'
+			+ '<p>deviceId</p>\n'
+			+ '</div></li>')[0];
+		$('strong', li).text(device.name != null ? device.name : device.id);
+		$('p', li).text(device.id);
+		li.dataset['devId'] = device.id;
+		li.dataset['dev'] = device;
+		$(li).addClass(device.state);
+		$(li).addClass('device');
+		$list.append(li);
+
+		if (device.state == 'online') onlineDevices += 1;
+
+		li.addEventListener('click', _clickHandler);
+		instance._items[device.id] = li;
+	}
+
+	$('#onlineCount').text(onlineDevices+'/'+data.devices.length);
 }
 
 class DeviceList {
@@ -25,44 +57,12 @@ class DeviceList {
 	}
 
 	reload() {
-		RestClient.listDevices(this._reload);
+		restClient.listDevices(_reload);
 	}
 
 	_deviceSelected() {
 		console.log('deviceSelected');
 		console.log(this);
-	}
-
-	_reload(data) {
-		var $list = $('#deviceList');
-		var onlineDevices = 0;
-
-		instance.clear();
-
-		for (var device of data.devices) {
-		//	console.log(device);
-
-			var li = $('<li class="list-group-item">\n'
-				+ '<span class="icon icon-monitor pull-left" style="font-size:24px;padding-right:5px" />\n'
-				+ '<div class="media-body">\n'
-				+ '<strong>device name</strong>\n'
-				+ '<p>deviceId</p>\n'
-				+ '</div></li>')[0];
-			$('strong', li).text(device.name != null ? device.name : device.id);
-			$('p', li).text(device.id);
-			li.dataset['devId'] = device.id;
-			li.dataset['dev'] = device;
-			$(li).addClass(device.state);
-			$(li).addClass('device');
-			$list.append(li);
-
-			if (device.state == 'online') onlineDevices += 1;
-
-			li.addEventListener('click', _clickHandler);
-			instance._items[device.id] = li;
-		}
-
-		$('#onlineCount').text(onlineDevices+'/'+data.devices.length);
 	}
 }
 
