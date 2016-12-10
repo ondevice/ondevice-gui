1f709a847a3f61e3d032fa39f5106e0d2b5538d1
Manuel Reithuber <git@manuel.reithuber.net>
2016-12-10 19:59:15 +0100

    fixed issue in main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-const photon = require('photon')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -9,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 300, height: 600})
+  win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
