11da52ae7ac226a87cd5ef5389f0a12c5c325ae4
Manuel Reithuber <git@manuel.reithuber.net>
2016-12-11 00:55:07 +0100

    RestClient: fetching the authentication every time (to allow user switching)

--- restClient.js
+++ restClient.js
@@ -3,14 +3,13 @@ var config = require('./config.js')
 
 class RestClient {
 	constructor() {
-		this.user=config.getClientUser();
-		this.auth=config.getClientAuth();
 	}
 
 	_getJSON(url, data, callback) {
 		// TODO don't hack together URLs
+		var auth = config.getClientUser()+':'+config.getClientAuth();
 		if (!url.startsWith('/')) throw "Error: 'url' has to start with a '/': "+url;
-		url = 'https://'+this.user+':'+this.auth+'@api.ondevice.io/v1.1' + url;
+		url = 'https://'+auth+'@api.ondevice.io/v1.1' + url;
 
 		return $.getJSON(url, data, callback);
 	}
